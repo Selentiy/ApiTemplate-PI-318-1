f9e1f6b6d4c31fc8519856d27d0af6e1bd61f5e5
Bogdan Selentiy <Bogdan.Selentiy@livenau.net>
2019-10-02 00:34:10 +0300

    Added Currency and ConversionRate classes

---
+++ App.Currencies/Models/ConversionRate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App.Currencies.Models
+{
+    public class ConversionRate
+    {
+        public IEnumerable<Currency> Currencies { get; set; }
+        public DateTime Date { get; set; }
+
+        public ConversionRate(DateTime date)
+        {
+            Currencies = new List<Currency>();
+            Date = date;
+        }
+    }
+}
---
+++ App.Currencies/Models/Currency.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Currencies.Models
+{
+    public class Currency
+    {
+        public string Name { get; set; }
+        public decimal Rate { get; set; }
+
+        public Currency(string name, decimal rate)
+        {
+            Name = name;
+            Rate = rate;
+        }
+    }
+}
