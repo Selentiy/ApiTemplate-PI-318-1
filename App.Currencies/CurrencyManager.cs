718450b9a933f305630c59a7c5211ff28925fbf3
Bogdan Selentiy <Bogdan.Selentiy@livenau.net>
2019-10-02 19:39:45 +0300

    Added CurrencyManager class which contains currencies business logic

---
+++ App.Currencies/CurrencyManager.cs
@@ -0,0 +1,35 @@
+﻿using App.Configuration;
+using App.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Currencies
+{
+    public interface ICurrencyManager
+    {
+        IEnumerable<string> GetCurrencyCodes();
+        decimal GetRate(string code, DateTime date);
+    }
+
+
+    public class CurrencyManager : ICurrencyManager, ITransientDependency
+    {
+        private readonly ICurrencyRepository _repository;
+
+        public CurrencyManager(ICurrencyRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<string> GetCurrencyCodes()
+        {
+            return _repository.GetCurrencyCodes();
+        }
+
+        public decimal GetRate(string code, DateTime date)
+        {
+            return _repository.GetRate(code, date);
+        }
+    }
+}
