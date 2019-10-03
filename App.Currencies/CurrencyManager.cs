bd224e5583d1a681e838d939f3b136340ca2c7a3
Bogdan Selentiy <Bogdan.Selentiy@livenau.net>
2019-10-03 17:00:05 +0300

    _exchangeRates is now local variable in GetExchangeRate method of CurrencyManager class

--- App.Currencies/CurrencyManager.cs
+++ App.Currencies/CurrencyManager.cs
@@ -14,8 +14,7 @@ namespace App.Currencies
 
     public class CurrencyManager : ICurrencyManager, ITransientDependency
     {
-        private readonly ICurrencyRepository _repository;
-        private Dictionary<string, decimal> _exchangeRates; 
+        private readonly ICurrencyRepository _repository; 
 
         public CurrencyManager(ICurrencyRepository repository)
         {
@@ -31,22 +30,22 @@ namespace App.Currencies
         {
             var result = new Dictionary<string, decimal>();
 
-            _exchangeRates = _repository.GetExchangeRates(date)
+            var exchangeRates = _repository.GetExchangeRates(date)
                 ?.ToDictionary(x => x.Key, x => x.Value);
 
-            if (_exchangeRates == null)
+            if (exchangeRates == null)
                 return null;
 
-            foreach (var rate in _exchangeRates)
-                result.Add(rate.Key, GetConversionRate(fromCode, rate.Key));
+            foreach (var rate in exchangeRates)
+                result.Add(rate.Key, GetConversionRate(exchangeRates, fromCode, rate.Key));
 
             result.Remove(fromCode);
             return result;
         }
 
-        private decimal GetConversionRate(string from, string to)
+        private decimal GetConversionRate(Dictionary<string, decimal> exchangeRates, string from, string to)
         {
-            decimal rate = _exchangeRates[from] / _exchangeRates[to];
+            decimal rate = exchangeRates[from] / exchangeRates[to];
             return rate;
         }
     }
