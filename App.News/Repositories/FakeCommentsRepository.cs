a75906d5dbc63ec3adf80745dafdb1ef30e39c01
Bohdan Zaritskyi <Bogdan.Zarickiy@livenau.net>
2019-10-10 21:33:07 +0300

    Added fake repositories

---
+++ App.News/Repositories/FakeCommentsRepository.cs
@@ -0,0 +1,77 @@
+﻿using App.Configuration;
+using App.Models.News;
+using App.Repositories.News;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace App.News.Repositories
+{
+    public class FakeCommentsRepository : ICommentsRepository
+    {
+        private IEnumerable<Comment> comments;
+
+        public FakeCommentsRepository()
+        {
+            comments = CommentsInitializer.GetComments();
+        }
+
+        public void CreateComment(Comment comment)
+        {
+            comments.Append(comment);
+        }
+
+        public Comment GetCommentById(int articleId, int commentId)
+        {
+            return comments.Where(cm => cm.ArticleID == articleId)
+                           .FirstOrDefault(cm => cm.CommentID == commentId);
+        }
+
+        public IEnumerable<Comment> GetComments()
+        {
+            return comments;
+        }
+
+        private static class CommentsInitializer
+        {
+            public static List<Comment> GetComments()
+            {
+                return new List<Comment>()
+                {
+                    new Comment()
+                    {
+                        ArticleID = 1,
+                        AuthorName = "Anonim",
+                        CommentID = 1,
+                        Content = "This is my anonim comment",
+                        Date = DateTime.Parse("01/18/2016 07:22:16")
+                    },
+                    new Comment()
+                    {
+                        ArticleID = 1,
+                        AuthorName = "Taras",
+                        CommentID = 2,
+                        Content = "I love dogs",
+                        Date = DateTime.Parse("02/13/2016 06:21:16")
+                    },
+                    new Comment()
+                    {
+                        ArticleID = 2,
+                        AuthorName = "Taras",
+                        CommentID = 1,
+                        Content = "I love cats",
+                        Date = DateTime.Parse("02/13/2016 06:23:06")
+                    },
+                    new Comment()
+                    {
+                        ArticleID = 3,
+                        AuthorName = "Taras",
+                        CommentID = 1,
+                        Content = "I love birds",
+                        Date = DateTime.Parse("02/13/2016 06:24:12")
+                    }
+                };
+            }
+        }
+    }
+}
