cc28ecb2a3560147c8b9d96b7b79e548e8dc241a
Bohdan Zaritskyi <Bogdan.Zarickiy@livenau.net>
2019-10-10 21:10:53 +0300

    Added models and repositories interfaces

---
+++ App.Common/Repositories/News/IArticlesRepository.cs
@@ -0,0 +1,11 @@
+﻿using App.Models.News;
+using System.Collections.Generic;
+
+namespace App.Repositories.News
+{
+    public interface IArticlesRepository
+    {
+        IEnumerable<Article> GetArticles();
+        Article GetArticleById(int id);
+    }
+}
