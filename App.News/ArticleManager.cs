5c034e4e8d7cc5ace57313f5333a327abb3697ad
Bohdan Zaritskyi <Bogdan.Zarickiy@livenau.net>
2019-10-10 21:57:33 +0300

    Added ArticleManager and CommentManager

---
+++ App.News/ArticleManager.cs
@@ -0,0 +1,31 @@
+﻿using App.Models.News;
+using App.Repositories.News;
+using System.Collections.Generic;
+
+namespace App.News
+{
+    public interface IArticleManager
+    {
+        IEnumerable<Article> GetArticles();
+        Article GetArticleById(int id);
+    }
+
+    public class ArticleManager : IArticleManager
+    {
+        readonly IArticlesRepository _repository;
+        public ArticleManager(IArticlesRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public Article GetArticleById(int id)
+        {
+            return _repository.GetArticleById(id);
+        }
+
+        public IEnumerable<Article> GetArticles()
+        {
+            return _repository.GetArticles();
+        }
+    }
+}
