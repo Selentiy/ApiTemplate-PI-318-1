fea50a0d628c3448d332b92db20f054c18b1bc25
BabenkoA <smaeel201356@gmail.com>
2019-10-13 17:00:12 +0300

    Add folders, an interface and a module

---
+++ App.RegularPayments/Interfaces/IRegularPayment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.RegularPayments.Interfaces
+{
+    public interface IRegularPayment
+    {
+        DateTime DateOfNextPayment();
+    }
+}
---
+++ App.RegularPayments/Models/RegularPayment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App.RegularPayments.Interfaces;
+
+namespace App.RegularPayments.Models
+{
+    class RegularPayment : IRegularPayment
+    {
+
+        public DateTime DateOfNextPayment()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
