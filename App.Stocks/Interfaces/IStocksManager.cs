928aff7556f2a89b9f8ca70117bc80eadddb8de4
Maria Moroz <dobitroy134@gmail.com>
2019-10-17 00:08:28 +0300

    add Interfaces folder
    +IStocksManager
    +ICompanyManager
    +ICompaniesRepository

---
+++ App.Stocks/Interfaces/IStocksManager.cs
@@ -0,0 +1,16 @@
+﻿using App.Configuration;
+using App.Repositories;
+using App.Stocks.ModelsView;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace App.Stocks.Interfaces
+{
+	public interface IStocksManager
+	{
+		Task<IEnumerable<StocksListItemView>> CompanyStocks(int companyId);
+		Task<StocksListItemView> CompanyStockByDate(int companyId, DateTime date);
+
+	}
+}
