691c56493cfae7b2e457acb506c8ee600ba05efc
Maria Moroz <dobitroy134@gmail.com>
2019-10-16 22:52:32 +0300

    add folder with Model and View
    +class Company
    +class CompanyView
    +class Stock
    +class OHCL_Candle
    +class StocksListItemView
    +class StocksListView

---
+++ App.Stocks/ModelsView/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Stocks.ModelsView
+{
+	public class Company
+	{
+		public long Org_Id { get; set; }
+		public string FullName { get; set; }
+		public string MainTicker { get; set; }
+
+		public string Description { get; set; }
+		public Stock[] Stocks { get; set; }
+	}
+
+	public class CompanyView
+	{
+		public long Org_Id { get; set; }
+		public string FullName { get; set; }
+		public string MainTicker { get; set; }
+
+		public string Description { get; set; }
+	}
+}
