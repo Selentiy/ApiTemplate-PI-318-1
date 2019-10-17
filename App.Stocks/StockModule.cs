0da3b45c4fb9ebf254531219eacc29c659798c6b
Maria Moroz <dobitroy134@gmail.com>
2019-10-17 12:02:15 +0300

    add StockModule

---
+++ App.Stocks/StockModule.cs
@@ -0,0 +1,21 @@
+﻿using App.Configuration;
+using App.Stocks.Interfaces;
+using App.Stocks.Services;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Stocks
+{
+	public class StockModule : IModule
+	{
+		public void Initialize(IWindsorContainer container)
+		{
+			container.Register(Component.For<IValidateServices>().ImplementedBy<ValidateServices>().LifestyleTransient());
+			container.Register(Component.For<ICompanyManager>().ImplementedBy<CompaniesManager>().LifestyleTransient());
+			container.Register(Component.For<IStocksManager>().ImplementedBy<StockManager>().LifestyleTransient());
+		}
+	}
+}
--- App.Web/Modules.cs
+++ App.Web/Modules.cs
@@ -6,7 +6,7 @@ namespace App.Web
     /// IMPORTANT ! In order to use classes and endpoints, defined in your own module, it should be referenced here as it shown
     /// </summary>
     [ModuleUsing(typeof(Example.ExampleModule))] // < ---- Example of module registration
-	[ModuleUsing(typeof(Example.StockModule))]
+	[ModuleUsing(typeof(Stocks.StockModule))]
 	public class Modules
     {
     }
