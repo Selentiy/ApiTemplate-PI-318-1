1e5692e2eb682e4ca76b8729a8ead84180bc124d
Kurimoire <55829187+Kurimoire@users.noreply.github.com>
2019-10-21 21:25:54 +0300

    Fix packages

--- App.Web/Modules.cs
+++ App.Web/Modules.cs
@@ -5,8 +5,9 @@ namespace App.Web
     /// <summary>
     /// IMPORTANT ! In order to use classes and endpoints, defined in your own module, it should be referenced here as it shown
     /// </summary>
-    [ModuleUsing(typeof(Example.ExampleModule))]
-    [ModuleUsing(typeof(Loans.LoansModule))]// < ---- Example of module registration
+    [ModuleUsing(typeof(Example.ExampleModule))] // < ---- Example of module registration
+    [ModuleUsing(typeof(Currencies.CurrenciesModule))]
+    [ModuleUsing(typeof(Loans.LoansModule))]
 
     public class Modules
     {
