e65bd688ebce41b8fa6ab89297f8e04abafe9471
MariaTeslia <56001501+MariaTeslia@users.noreply.github.com>
2019-10-22 12:32:26 +0300

    Revert "Make controller"
    This reverts commit d8a2c92c70977222ba807d459123021ba2324298.

--- App.Users/Controllers/UsersController.cs
+++ App.Users/Controllers/UsersController.cs
@@ -1,37 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using App.Users.Services;
-using App.Users.Repository;
+
 namespace App.Users.Controllers
 {
-    [Route("api/Users")]
-    [ApiController]
-    public class UsersController : ControllerBase
+    public class UsersController
     {
-        readonly IUsersManager _usersManager;
-        public UsersController(
-            IUsersManager usersManager)
-        {
-            _usersManager = usersManager;
-        }
 
-        [HttpGet("active")]
-        public ActionResult<IEnumerable<string>> GetListActiveUers()
-        {
-            var serviceCallResult = _usersManager.GetListActiveUsers().ToList();
-            return serviceCallResult;
-        }
-
-        [HttpPost]
-        public void ResetPassword(int id, string pass)
-        {
-            _usersManager.ResetPassword(id, pass);
-        }
-        [HttpPost]
-        public void BlockOrUnblockUserById(int id)
-        {
-            _usersManager.BlockOrUnblockUserById(id);
-        }
     }
 }
