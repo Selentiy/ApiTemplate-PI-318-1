94a4c6e169aff61cbe6163c2198fffe4d82dca7d
MariaTeslia <56001501+MariaTeslia@users.noreply.github.com>
2019-10-22 11:21:58 +0300

    Create classes, create model and Repository

---
+++ App.Users/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Users.Models
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        //if true-unblocked
+        public bool BlockStatus { get; set; }
+        
+        public User(string login,string pass)
+        {
+            this.BlockStatus = true;
+            this.Login = login;
+            this.Password = pass;
+        }
+        public override string ToString()
+        {
+            return $"User Login: {Login}" + (BlockStatus == true? " Active user " : " User are blocked. ");
+        }
+    }
+}
---
+++ App.Users/Repository/InMemoryUsersRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App.Users.Models;
+
+namespace App.Users.Repository
+{   public interface IInMemoryUsersRepository
+    {
+        IEnumerable<string> GetStringListActiveUsers();
+        User Get(int id);
+
+    }
+
+    public class InMemoryUsersRepository : IInMemoryUsersRepository
+    {
+        User[] users = new User[5];
+        public InMemoryUsersRepository()
+        {
+            users[0] = new User("docent", "1212");
+            users[1] = new User("webterror", "vikaanik");
+            users[2] = new User("moki", "marmok");
+            users[3] = new User("AntonBeetroot", "full-kek");
+            users[4] = new User("Vitor", "parol");
+        }
+        public User Get(int id)
+        {
+            return users[id];
+        }
+
+        public IEnumerable<string> GetStringListActiveUsers()
+        {
+            string[] str = new string[5];
+            str[0] = users[0].ToString();
+            str[1] = users[1].ToString();
+            str[2] = users[2].ToString();
+            str[3] = users[3].ToString();
+            str[4] = users[4].ToString();
+            return str;
+        }
+    }
+}
