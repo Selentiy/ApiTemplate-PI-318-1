76244b6d77acd9adc99ff748831f693e40e9fdf1
bogdan-chex <bogdanchex@gmail.com>
2019-10-22 23:37:07 +0300

    added account model

---
+++ App.Common/Models/Accounts/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace App.Models.Accounts
+{
+    public class Account
+    {
+        public string CountryCode { get; set; }
+        public string CheckDigits { get; set; }
+        public string BankCode { get; set; }
+        public string AccountNumber { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+        public bool IsBlocked { get; set; }
+        public string Owner { get; set; }
+    }
+}
