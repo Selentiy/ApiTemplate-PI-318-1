b9b377b031b0220c227398920152ee39069ecce1
Maria Moroz <dobitroy134@gmail.com>
2019-10-23 23:19:20 +0300

    remove useless classes

--- App.Stocks/StockModule.cs
+++ App.Stocks/StockModule.cs
@@ -1,11 +1,5 @@
 ﻿using App.Configuration;
-using App.Stocks.Interfaces;
-using App.Stocks.Services;
-using Castle.MicroKernel.Registration;
 using Castle.Windsor;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace App.Stocks
 {
@@ -13,7 +7,7 @@ namespace App.Stocks
 	{
 		public void Initialize(IWindsorContainer container)
 		{
-			
+
 		}
 	}
 }
