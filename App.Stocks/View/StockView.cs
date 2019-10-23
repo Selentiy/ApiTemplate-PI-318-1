1237d150c6bf6b7bbc36f44dc34f0b03dd930488
Maria Moroz <dobitroy134@gmail.com>
2019-10-23 23:15:19 +0300

    fix model and view

---
+++ App.Stocks/View/StockView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Stocks.View
+{
+	public class StocksListView
+	{
+		public IEnumerable<StocksListItemView> Stocks { get; set; }
+		public string Company_Name { get; set; }
+	}
+
+	public class StocksListItemView
+	{
+		public string Ticker { get; set; }
+		public bool IsTraded { get; set; }
+
+		public decimal Open { get; set; }
+		public decimal High { get; set; }
+		public decimal Close { get; set; }
+		public decimal Low { get; set; }
+
+		public DateTime Date { get; set; }
+	}
+}
