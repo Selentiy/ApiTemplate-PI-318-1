9c2ad9feb5ab6b8d79e0662f6309e58769bede02
Bogdan Selentiy <Bogdan.Selentiy@livenau.net>
2019-10-30 23:55:23 +0200

    Added FutureDateException class in case the date has not yet come. Added CurrencyCodeException class in case the currency code format will be incorrect.

---
+++ App.Currencies/Exceptions/CurrencyCodeFormatException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Currencies.Exceptions
+{
+    public class CurrencyCodeFormatException : Exception
+    {
+        public string CurrencyCode { get; private set; }
+
+        public CurrencyCodeFormatException() { }
+
+        public CurrencyCodeFormatException(string currencyCode)
+        {
+            CurrencyCode = currencyCode;
+        }
+
+        public CurrencyCodeFormatException(string message, Exception innerException)
+            : base(message, innerException) { }
+
+        public CurrencyCodeFormatException(string message, string currencyCode) : base(message)
+        {
+            CurrencyCode = currencyCode;
+        }
+    }
+}
---
+++ App.Currencies/Exceptions/FutureDateException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Currencies.Exceptions
+{
+    public class FutureDateException : Exception
+    {
+        public DateTime Date { get; private set; }
+
+        public FutureDateException() { }
+
+        public FutureDateException(DateTime date)
+        {
+            Date = date;
+        }
+
+        public FutureDateException(string message) : base(message) { }
+
+        public FutureDateException(string message, DateTime date) : base(message)
+        {
+            Date = date;
+        }
+
+        public FutureDateException(string message, Exception innerException) 
+            : base(message, innerException) { }
+    }
+}
