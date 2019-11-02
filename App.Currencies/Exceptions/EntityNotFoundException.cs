9fc74168cfd42b5575b65d3c1c49dd1a8e680b58
Bogdan Selentiy <Bogdan.Selentiy@livenau.net>
2019-11-02 22:03:17 +0200

    Added EntityNotFoundException class.

---
+++ App.Currencies/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Currencies.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public Type EntityType { get; private set; }
+        public EntityNotFoundException(Type entityType)
+        {
+            EntityType = entityType;
+        }
+    }
+}
