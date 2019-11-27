fe593dae533e23458f54c7f8bc9caafcd9e9d24b
Oleg Demihov <Oleg.Demihov@livenau.net>
2019-11-27 19:57:00 +0200

    Created CardsDbContext and registered it in DI

---
+++ App.Cards/Database/CardsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using App.Models.Cards;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Cards.Database
+{
+    public class CardsDbContext: DbContext
+    {
+        public DbSet<Card> Cards { get; set; }
+        public CardsDbContext(DbContextOptions<CardsDbContext> options) : base(options) { }
+        
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            
+        }
+    }
+}
