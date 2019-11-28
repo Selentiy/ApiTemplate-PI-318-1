39b08d9bc207cd2b5bf62d911770bb61fa3b0da4
Maria Moroz <dobitroy134@gmail.com>
2019-11-28 10:31:12 +0200

    Changes in model class

--- App.Common/Models/Stocks/Company.cs
+++ App.Common/Models/Stocks/Company.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace App.Models.Stocks
 {
 	public class Company
 	{
-		public long OrgId { get; set; }
+		[Key]
+		public int OrgId { get; set; }
 		public string FullName { get; set; }
 		public string MainTicker { get; set; }
 		
 		public string Description { get; set; }
-		public List<Stock> Stocks { get; set; }
+		public ICollection<Stock> Stocks { get; set; }
 	}
 }
--- App.Common/Models/Stocks/Stock.cs
+++ App.Common/Models/Stocks/Stock.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace App.Models.Stocks
 {
 	public class Stock
 	{
+		[Key]
+		public int Key { get; set; }
 		public string Ticker { get; set; }
 
 		public bool IsTraded { get; set; }
 		public OHCLCandle Candle { get; set; }
+		//public int CompanyId { get; set; }
+		public virtual Company Company { get; set; }
 
 		public bool CompareDate(DateTime date) => date.ToShortDateString()
 			.CompareTo(Candle.Date.ToShortDateString()) == 0;
@@ -18,11 +24,16 @@ namespace App.Models.Stocks
 	/// </summary>
 	public class OHCLCandle 
 	{
+		[Key]
+		public int CandleKey { get; set; }
+
 		public decimal Open { get; set; }
 		public decimal High { get; set; }
 		public decimal Close { get; set; }
 		public decimal Low { get; set; }
 
 		public DateTime Date { get; set; }
+		public int StockRef { get; set; }
+		public Stock Stock { get; set; }
 	}
 }
