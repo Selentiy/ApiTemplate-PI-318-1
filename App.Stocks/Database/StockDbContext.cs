0ecaad9e64b61833b798fa9d1940c90f0a60b3dd
Maria Moroz <dobitroy134@gmail.com>
2019-11-28 10:32:54 +0200

    Create StockDbContext and register it

---
+++ App.Stocks/Database/StockDbContext.cs
@@ -0,0 +1,28 @@
+﻿using App.Models.Stocks;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Stocks.Database
+{
+	public class StockDbContext : DbContext
+	{
+		public DbSet<Company> Companies { get; set; } 
+		public DbSet<Stock> Stocks { get; set; }
+		public StockDbContext(DbContextOptions<StockDbContext> options) : base(options) { }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+
+			builder.Entity<Company>()
+				.HasMany(a => a.Stocks).WithOne(b => b.Company)
+				//.HasForeignKey(s => s.CompanyId)
+				.HasPrincipalKey(c => c.OrgId);
+			builder.Entity<Stock>()
+				.HasOne(a => a.Candle).WithOne(b => b.Stock)
+				.HasForeignKey<OHCLCandle>(o => o.StockRef)
+				.HasPrincipalKey<Stock>(s => s.Key);
+		}
+	}
+}
