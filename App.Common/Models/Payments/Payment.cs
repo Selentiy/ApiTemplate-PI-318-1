41362747c504b3c51b66d6c22dda92124a2111a0
Бабенко Андрей <BabenkoA@AREON.LOCAL>
2019-12-18 16:44:14 +0200

    Create the project and add model

---
+++ App.Common/Models/Payments/Payment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Models.Payments
+{
+    public enum Status 
+    {
+        Success = 1,
+        Failure,
+        Reversed
+    }
+    public class Payment
+    {
+        public int ID { get; set; }
+        public string Payer { get; set; }
+        public string Recipient { get; set; }
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+        public Status Status { get; set; }
+    }
+}
